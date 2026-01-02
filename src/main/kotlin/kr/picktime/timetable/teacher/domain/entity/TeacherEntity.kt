dd49f1badb81b2ad7f2250cf92e440ca548fe0f8
su080915 <tbr335@naver.com>
2026-01-02 17:05:20 +0900

    fix: add id annotation in teacher entity

--- src/main/kotlin/kr/picktime/timetable/teacher/domain/entity/TeacherEntity.kt
+++ src/main/kotlin/kr/picktime/timetable/teacher/domain/entity/TeacherEntity.kt
@@ -1,10 +1,12 @@
 package kr.picktime.timetable.teacher.domain.entity
 
 import kr.picktime.timetable.teacher.domain.enum.TeacherType
+import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("teachers")
 data class TeacherEntity(
+    @Id
     val id: Long? = null,
     val schoolId: Long,
     val name: String,
